9dc4a2b41557e4ead64eba5996b201618677e9b3
Paul <twinencrypt@gmail.com>
2024-08-26 18:46:34 +0700

    Add parameter to engine sqlalchemy

--- database/database.py
+++ database/database.py
@@ -10,7 +10,7 @@ load_dotenv(find_dotenv())
 def get_engine():
     return create_engine(
         f"postgresql+psycopg2://{os.getenv('PG_USER')}:{os.getenv('PG_PASSWORD')}"
-        f"@{os.getenv('PG_HOST')}:{os.getenv('PG_PORT')}/{os.getenv('PG_DB')}")
+        f"@{os.getenv('PG_HOST')}:{os.getenv('PG_PORT')}/{os.getenv('PG_DB')}", pool_pre_ping=True)
 
 
 def create_db():
